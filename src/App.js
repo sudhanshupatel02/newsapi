299424746f6d909473e17241f1b6d1701bfb308c
Sudhanshu Patel <sudhanshupatel0207@gmail.com>
2023-05-05 17:06:50 +0530

    class based comp 23

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 
 import React, { Component } from 'react'
-
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
-        Helloo my first class based component{this.c}
+        <NavBar/>
+        <News/>
       </div>
     )
   }
---
+++ src/components/News.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        This is a news Component
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+      </div>
+    )
+  }
+}
+
+export default News
+
+
