18c88572130e4ed2723e18f29b8bc1b01d8b6cdf
Sudhanshu Patel <sudhanshupatel0207@gmail.com>
2023-05-05 15:13:00 +0530

    class based comp

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 
 import React, { Component } from 'react'
-import NavBar from './components/NavBar';
-import News from './components/News';
+
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
-        <NavBar/>
-        <News/>
+        Helloo my first class based component{this.c}
       </div>
     )
   }
